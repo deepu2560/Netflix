4077984c51adb4d3ce8be1cdd44e880966ce8209
mohan_palleti <palletimohanbabu23@gmail.com>
2022-03-30 23:06:52 +0530

    user controller

--- auth/index.js
+++ auth/index.js
@@ -4,14 +4,16 @@ app.use(express.json());
 const connect = require("./configs/db");
 const dotenv = require("dotenv");
 const authRout = require("./controllers/auth");
+const userRoute = require("./controllers/user_controller");
 dotenv.config();
 
 app.use("/api/auth", authRout);
+app.use("/api/users", userRoute);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, async () => {
   try {
     await connect();
-    console.log(`Server is running on ${PORT} bitch!!`);
+    console.log(`Server is running  bitch!!`);
   } catch (err) {
     console.log(err);
   }
