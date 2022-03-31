97aab9f5089939b2bd5ec94dc2fd58ead3a581e6
mohan_palleti <palletimohanbabu23@gmail.com>
2022-03-31 10:21:41 +0530

    routing

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -1,15 +1,42 @@
 import "./app.scss";
 import { Login } from "./Components/login/Login";
 import { Register } from "./Components/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
 import Home from "./HomePage/Home";
 
 function App() {
+  let user = false;
   return (
-    // <div className="App">
-
-    // </div>
-    // <Register />
-    <Home />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Redirect to="/register" />}
+        </Route>
+        <Route path="/register">
+          {!user ? <Register /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+        {user && (
+          <>
+            <Route path="/movies">
+              <Home type="movie" />
+            </Route>
+            <Route path="/series">
+              <Home type="series" />
+            </Route>
+            <Route path="/watch">
+              <Watch />
+            </Route>
+          </>
+        )}
+      </Switch>
+    </Router>
   );
 }
 
