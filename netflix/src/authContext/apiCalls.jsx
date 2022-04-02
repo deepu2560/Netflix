cd1ec49730a3d301c57ede7111ec3228452b5572
mohan_palleti <palletimohanbabu23@gmail.com>
2022-04-02 14:43:05 +0530

    authentication

--- netflix/src/authContext/apiCalls.jsx
+++ netflix/src/authContext/apiCalls.jsx
@@ -4,9 +4,13 @@ import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post("http://localhost:8080/api/auth/login", user);
+    const res = await axios.post(
+      "https://mycrosproxy-forauth.herokuapp.com/https://netflix-backend23.herokuapp.com/api/auth/login",
+      user
+    );
     dispatch(loginSuccess(res.data));
   } catch (err) {
+    console.log("erroe", err);
     dispatch(loginFailure());
   }
 };
