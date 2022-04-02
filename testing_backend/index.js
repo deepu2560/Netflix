15dd968b8c26ff977125ac3a1f46e25e4312c71a
mohan_palleti <palletimohanbabu23@gmail.com>
2022-04-02 18:24:07 +0530

    final back-end deployed

--- testing_backend/index.js
+++ testing_backend/index.js
@@ -1,26 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
+const connect = require("./config/db");
 var cors = require("cors");
 
 app.use(cors());
-const connect = require("./configs/db");
-const dotenv = require("dotenv");
-const authRout = require("./controllers/auth");
 const userRoute = require("./controllers/user_controller");
 const movieRoute = require("./controllers/movie_controller");
 const listRoute = require("./controllers/list_controller");
-dotenv.config();
-
-app.use("/api/auth", authRout);
+const user_auth = require("./controllers/auth");
+app.use("/user", user_auth);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
   try {
     await connect();
-    console.log(`Server is running!!`);
+    console.log(`Server is running on ${PORT}`);
   } catch (err) {
     console.log(err);
   }
